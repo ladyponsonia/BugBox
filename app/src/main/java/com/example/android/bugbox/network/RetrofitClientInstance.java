4c3ff9baffd6fbc6efe33bd2e45f50fccc3d42b8
ladyponsonia <ladyponsonia@gmail.com>
2018-09-23 11:58:46 -0700

    updating recycler view ..wip..

--- app/src/main/java/com/example/android/bugbox/network/RetrofitClientInstance.java
+++ app/src/main/java/com/example/android/bugbox/network/RetrofitClientInstance.java
@@ -8,6 +8,7 @@ public class RetrofitClientInstance {
     private static Retrofit retrofit;
     private static final String BASE_URL = "https://poly.googleapis.com/";
 
+
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
