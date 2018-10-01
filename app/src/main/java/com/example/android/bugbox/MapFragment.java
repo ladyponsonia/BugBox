5b5dea95ef0e925ba466733a4aee31304daa3d13
ladyponsonia <ladyponsonia@gmail.com>
2018-09-30 17:14:18 -0700

    added progress bar to map

--- app/src/main/java/com/example/android/bugbox/MapFragment.java
+++ app/src/main/java/com/example/android/bugbox/MapFragment.java
@@ -1,13 +1,15 @@
 package com.example.android.bugbox;
 
-import android.content.Context;
-import android.net.Uri;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.android.bugbox.model.Bug;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,47 +29,21 @@ import java.util.ArrayList;
  * https://www.journaldev.com/10380/android-google-maps-example-tutorial
  */
 public class MapFragment extends Fragment implements OnMapReadyCallback {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final int ACCESS_FINE_LOCATON_CODE = 444;
 
     private GoogleMap mMap;
-    //private OnFragmentInteractionListener mListener;
+    private SupportMapFragment mMapFragment;
+    private ProgressBar mProgressBar;
+
 
     public MapFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -76,46 +52,33 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         // Inflate the layout for this fragment
         View rootview = inflater.inflate(R.layout.fragment_map, container, false);
 
+        //get views
+        mProgressBar = rootview.findViewById(R.id.map_progress_bar);
+        mProgressBar.setVisibility(View.VISIBLE);
+        mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.setUserVisibleHint(false);
+        Log.d("MAPfragment", "onCreateView" + mMapFragment.toString());
+
         //start map
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        Log.d("MAPfragment", "onCreateView" + mapFragment.toString());
-        mapFragment.getMapAsync(this);
+        mMapFragment.getMapAsync(this);
         return rootview;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    /*
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }*/
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        /*if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }*/
-
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-       // mListener = null;
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //check/request location permissions
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATON_CODE);
+            return;
+        }
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setZoomGesturesEnabled(true);
-        mMap.getUiSettings().setMapToolbarEnabled(false);
-        //mMap.setMyLocationEnabled(true); need to request runtime permission
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.setMyLocationEnabled(true);
 
         //get bugs data
         ArrayList<Bug> bugsList = MainActivity.bugsList;
@@ -128,22 +91,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         //center camera on 1st bug
         mMap.moveCamera(CameraUpdateFactory
                 .newLatLngZoom(new LatLng(bugsList.get(0).getLat(), bugsList.get(0).getLon()), 12));
+        mProgressBar.setVisibility(View.INVISIBLE);
+        mMapFragment.setUserVisibleHint(true);
     }
 
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
